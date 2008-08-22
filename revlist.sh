a1444635d5e55dabe6204536ec5f6db0706ed378
Avery Pennarun <apenwarr@gmail.com>
2008-08-22 18:52:26 -0400

    revlist: never print an ignored commit at all.
    Previously we would print the ignored commit and then stop; actually, we
    should be skipping it completely, or it doesn't actually make ignored
    branches disappear like it should.

--- revlist.sh
+++ revlist.sh
@@ -6,8 +6,13 @@ cd "$DIR/build"
 #	sed -e 's,^\(.*/\)*\([0-9a-f]*\).*$,^\2^,g' |
 git rev-list --first-parent --pretty=oneline "$@" |
 	while read commit comment; do
+		if [ -f ../out/ignore/$commit ]; then
+			# never print an ignored commit
+			exit 0;
+		fi
 		echo "$commit $comment"
-		if [ -f ../out/pass/$commit -o -f ../out/ignore/$commit ]; then
+		if [ -f ../out/pass/$commit ]; then
+			# print the first passing commit, then done
 			exit 0;
 		fi
 	done
