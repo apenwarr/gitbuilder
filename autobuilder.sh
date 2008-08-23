9feb3c4b2ca550eaee3abd8243ddc8b27207c200
Avery Pennarun <apenwarr@gmail.com>
2008-08-22 22:06:05 -0400

    autobuilder: print a helpful error message if build.sh doesn't exist.
    Also, git 'git remote update' every time through the main branch loop.  If
    someone fixes a bug on a branch, there's no point continuing to bisect that
    bug ad nauseam; it's much better to compile the new versions first of all.

--- autobuilder.sh
+++ autobuilder.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+DIR="$(dirname $0)"
+cd "$DIR"
+
+if [ -e build.sh -a ! -x build.sh ]; then
+	chmod a+x build.sh
+fi
+
+if [ ! -x build.sh ]; then
+	echo >&2
+	echo "We need an executable file named build.sh in this directory" >&2
+	echo "in order to run the autobuilder." >&2
+	echo >&2
+	echo "Try copying build.sh.example as a starting point." >&2
+	echo >&2
+	exit 1
+fi
 
 mkdir -p out/pass out/fail out/ignore
 
-( cd build && git remote update )
-
-if [ -n "$*" ]; then
-	branches="$*"
-else
-	branches=$( (cd build && git-branch -r) )
-fi
-
 did_something=1
 while [ -n "$did_something" ]; do
+	( cd build && git remote update )
 	did_something=
 	for branch in $(./branches.sh | sed 's/^[0-9a-f]* //'); do
 		ref=$(./next-rev.sh $branch)
