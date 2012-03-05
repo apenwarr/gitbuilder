2371e2ecac0432753727744afd9a9bb6837a2011
matthias van wambeke <matthias.van.wambeke@howest.be>
2012-03-05 16:42:12 +0000

    Added

---
+++ contrib/gitBuilder-ajax/js/urls.js
@@ -0,0 +1,12 @@
+/**
+ * @author mvanwamb
+ */
+ urls = new Array(
+			    'http://localhost/view/test.htm',
+			    'http://localhost/view/test2.htm',
+			    'http://localhost/view/test3.htm',
+			    'http://localhost/view/test4.htm'
+				);
+				
+serverUrl = "http://localhost/view/request.php";
+
