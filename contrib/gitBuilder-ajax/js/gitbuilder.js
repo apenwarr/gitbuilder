69f4921ec91206c8dc1aa75b7cfececdaddecc3e
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-03-06 09:50:52 +0000

    remove hide/show buttons, these buttons clutter things

--- contrib/gitBuilder-ajax/js/gitbuilder.js
+++ contrib/gitBuilder-ajax/js/gitbuilder.js
@@ -17,15 +17,11 @@ function summarizeUrls(link, i){
 			var d = d.html;
 			summaryRow(link, d);
 			checkStatus();
-			makeShowLinks();
-			makeHideLinks();
 		});
 	} else {
 		$.get(link,function(d){
 			summaryRow(link, d);
 			checkStatus();
-			makeShowLinks(link);
-			makeHideLinks();
 		});
 	}
 }
@@ -34,45 +30,8 @@ function summarizeUrls(link, i){
 function summaryRow(link, d) {
 	var mostrecent = $(d).filter("#most_recent").html().replace("Most Recent:","").replace(/<a href="#/g,'<a href="' + link + '#');
 	var url = "<a class='normalLink' href='" + link + "'>" + link + "</a>";
-	var btn = "<a title='Show it here' href='" + link + "' class='btn showDetails'>Show</a>";
-	var btn2 = "<a title='hide it' href='#' class='btn hideBtn'>Hide</a>";
-	var row = "<tr><td>" + url + "</td><td><div class='most_recent'>" + mostrecent + "</div></td><td class='butColumn'>" + btn + btn2 + "</td></tr>";
+	var row = "<tr><td>" + url + "</td><td><div class='most_recent'>" + mostrecent + "</div></td></tr>";
 	$("#links tbody").append(row);
-}
-
-
-function makeHideLinks(){
-	$(".hideBtn").click(function(){
-		$(".empt").empty();
-		$(this).hide();
-		$(this).prev().show();
-	})
-}
-
-
-function makeShowLinks(link){
-	$(".showDetails").click(function(event){
-		event.preventDefault();
-		
-		$(this).hide();
-		$(this).next().show();
-		
-		$(".empt").empty();
-		if (typeof serverUrl !== 'undefined') {
-			var data = serverUrl + "?url=" + $(this).attr("href") + "&callback=?";
-			$.getJSON(data,function(d){
-				var d = d.html;
-				$(".empt").append(d);
-				$(".empt table").addClass("table table-striped table-bordered");
-			});
-		} else {
-			var data = $(this).attr("href");
-			$.get(data,function(d){
-				$(".empt").append(d);
-				$(".empt table").addClass("table table-striped table-bordered");
-			});
-		}
-	});
 }
 
 
