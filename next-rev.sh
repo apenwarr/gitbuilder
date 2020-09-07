dcf4f468d931ab93fe9cd2860be86a211de214ac
Avery Pennarun <apenwarr@tailscale.com>
2020-09-07 04:46:04 -0400

    next-rev.sh: algorithm error would end bisection too early.
    Technically, this was correct because we were searching for the "first
    failing commit" (ie. the one that caused a transition from pass to
    fail). However, in case there are multiple transitions because of a failing
    commit in the middle, we should keep testing even after we find the
    first failure.

--- next-rev.sh
+++ next-rev.sh
@@ -15,16 +15,19 @@ bisect()
 
 ../revlist.sh "$@" | (
 	pass=
+	firstfail=
 	fail=
 	pending=
 	while read commit junk; do
-		if [ -e ../out/pass/$commit ]; then
+		if [ -e "../out/pass/$commit" ]; then
 			# only a maximum of one pass is ever received
 			pass=$commit
-		elif [ -e ../out/fail/$commit ]; then
+		elif [ -e "../out/fail/$commit" ]; then
 			# there might be more than one fail; we want the
-			# last one
+			# last one, so that we can figure out where things
+			# started to go wrong.
 			fail=$commit
+			[ -n "$firstfail" ] || firstfail=$commit
 		elif [ -z "$pending" -a -z "$fail" ]; then
 			# and we only want the first pending build,
 			# and only if it's *not* following a failed build
@@ -33,16 +36,31 @@ bisect()
 		last=$commit
 	done
 	
-	# if a pending build came before the first failed build, then we need to
-	# build it first.
 	if [ -n "$pending" ]; then
-		echo $pending
-	elif [ -n "$fail" -a -n "$pass" ]; then
-		bisect $fail^ ^$pass
-	elif [ -n "$fail" -a "$last" != "$fail" -a -n "$last" ]; then
-		bisect $fail^ ^$last
+		# if a pending build came before the first failed build,
+		# then we need to build it first.
+		echo "$pending"
+	elif [ -n "$fail" ]; then
+		# If there were no passing tests, just use the last one as
+		# a reference.
+		[ -n "$pass" ] || pass=$last
+
+		# First, we want to bisect to find the *last* failure before
+		# the first pass. That is, the commit where failures were
+		# introduced.
+		try=$(bisect "$fail^" "^$pass")
+
+		# But if there's nothing left to try (we tested it already),
+		# let's keep building the other commits in between, just in
+		# case one of the intermediate ones passes. (This can happen
+		# if commits are failing for two different reasons, sigh.)
+		if [ -z "$try" ]; then
+			try=$(bisect "$firstfail^" "^$pass")
+		fi
+		if [ -n "$try" ]; then
+			echo "$try"
+		fi
 	fi
 
 	# if we don't print anything at all, it means there's nothing to build!
 )
-
